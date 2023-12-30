9347a202243f5583493f344660f369f67eb54060
Rithk <rithkchaudhary86@gmail.com>
2023-12-30 11:24:24 +0530

    dsa

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -1,54 +1,45 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int n = 100;
-class queue
-{
-    public:
-    int *arr;
+int n=100;
+class queue{
+    int* arr;
     int front;
     int back;
-
-    queue()
-    {
-        arr = new int[n];
-        front = -1;
-        back = -1;
+    public:
+    queue(){
+    arr=new int[n];
+    front = -1;
+    back=-1;
     }
-    void push(int x)
-    {
-        if (back == (n - 1))
-        {
-            cout << "queue overflow" << endl;
-            return;
+    void push(int x){
+        if(back==(n-1)){
+            cout<<"queue is full"<<endl;
+           
         }
         back++;
-        arr[back] = x;
-        if (front == -1)
-        {
+        arr[back]=x;
+        if(front==-1){
             front++;
         }
     }
-    void pop()
-    {
-        if (front == -1 || front > back)
-        {
-            cout << "no element in queue" << endl;
+    void pop(){
+        if(front==-1 || front>back){
+            cout<<"queue is empty"<<endl;
+            return ;
         }
         front++;
     }
-    int peek()
-    {
-        if (front == -1 || front > back)
-        {
-            cout << "no element in queue" << endl;
+    int peek(){
+       if(front==-1 || front>back){
+            cout<<"queue is empty"<<endl;
             return -1;
         }
-        return arr[front];
+        else{
+            return arr[front];
+        }
     }
-    bool empty()
-    {
-        if (front == -1 || front > back)
-        {
+    bool empty(){
+        if(front==-1){
             return true;
         }
         return false;
@@ -56,20 +47,18 @@ class queue
 };
 int main()
 {
-    queue q;
-    q.push(5);
-    q.push(4);
-    q.push(3);
-    q.push(2);
-    q.push(1);
-
-    cout << q.peek() << endl;
-    q.pop();
-    cout << q.peek() << endl;
-    q.pop();
-    cout << q.peek() << endl;
-    q.pop();
-    cout << q.peek() << endl;
-    q.pop();
-    return 0;
+    queue q1;
+    q1.push(1);
+    q1.push(2);
+    q1.push(3);
+    q1.push(4);
+    cout<<q1.peek()<<endl;
+    q1.pop();
+    cout<<q1.peek()<<endl;
+    q1.pop();
+    cout<<q1.peek()<<endl;
+    q1.pop();
+    cout<<q1.peek()<<endl;
+    q1.pop();
+ return 0;
 }
